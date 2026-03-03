b503aeff52f6415f2f4decebffd38ee3c6fec032
Lukasz Piwnik <s31827@pjwstk.edu.pl>
2026-03-03 19:33:53 +0100

    submit 2 task

--- TPO1_PL_S31827/src/zad2/Futil.java
+++ TPO1_PL_S31827/src/zad2/Futil.java
@@ -1,4 +1,41 @@
 package zad2;
 
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+
+
+import static java.nio.file.StandardOpenOption.*;
+
+
 public class Futil {
+    public static void processDir(String dirName, String resultFileName) {
+
+        try (FileChannel channel = FileChannel.open(Paths.get(resultFileName), CREATE, TRUNCATE_EXISTING, WRITE)) {
+
+            Files.walkFileTree(Path.of(dirName), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    final List<String> lines = Files.readAllLines(file, Charset.forName("Cp1250"));
+
+                    String content = String.join("\n", lines) + "\n";
+
+                    channel.write(StandardCharsets.UTF_8.encode(content));
+
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
--- TPO1_PL_S31827/src/zad2/Main.java
+++ TPO1_PL_S31827/src/zad2/Main.java
@@ -7,10 +7,12 @@
 package zad2;
 
 
-//public class Main {
-//  public static void main(String[] args) {
-//    String dirName = System.getProperty("user.home")+"/TPO1dir";
-//    String resultFileName = "TPO1res.txt";
-//    Futil.processDir(dirName, resultFileName);
-//  }
-//}
+public class Main {
+  public static void main(String[] args) {
+    String dirName = System.getProperty("user.home")+"/TPO1dir";
+    String resultFileName = "TPO1res.txt";
+    Futil.processDir(dirName, resultFileName);
+
+
+  }
+}
